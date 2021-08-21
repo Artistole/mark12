97268ccb2db64f537a3050df5589493d633b6cb1
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2021-08-21 23:00:55 +0530

    refactor

--- area.js
+++ area.js
@@ -1,18 +1,39 @@
-const inputs = document.querySelectorAll(".area-input");
+// const inputs = document.querySelectorAll(".area-input");
+// const areaBtn = document.querySelector("#area-btn");
+// const areaOutput = document.querySelector("#area-output");
+
+// function multiplyBaseAndHeight(base, height) 
+// {
+// 	const multipliedOutput = base * height;
+// 	return multipliedOutput;
+// }
+
+// function calculateArea() 
+// {
+// 	//area = (base * height)/2
+// 	const basexheight = multiplyBaseAndHeight(Number(inputs[0].value), Number(inputs[1].value));
+// 	const Area = basexheight/2;
+// 	areaOutput.innerText = `The area of the triangle is`  ;
+// }
+
+// areaBtn.addEventListener('click', calculateArea);
+
+const areas = document.querySelectorAll(".area-input");
 const areaBtn = document.querySelector("#area-btn");
-const areaOutput = document.querySelector("#area-output");
+const outputE1 = document.querySelector("#output");
 
-function multiplyBaseAndHeight(base, height) {
-	const multipliedOutput = base * height;
-	return multipliedOutput;
-}
-
-function calculateArea() 
+function multiplication(a,b)
 {
-	//area = (base * height)/2
-	const basexheight = multiplyBaseAndHeight(Number(inputs[0].value), Number(inputs[1].value));
-	const area = basexheight / 2;
-	areaOutput.innerText = `The area of the triangle is` + calculateArea ;
+    const mOutput = a*b ; 
+    return mOutput;
 }
 
-areaBtn.addEventListener('click', calculateArea);
+
+function calculateArea()
+{
+  const mOutput = multiplication(Number(areas[0].value), Number(areas[1].value));
+  const Area = mOutput/2;
+  outputE1.innerText = "The area of the triangle is  " + Area + "cm²";
+}
+
+areaBtn.addEventListener("click", calculateArea);
